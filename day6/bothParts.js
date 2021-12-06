cce27f17d554315c20ae9dfd57f63cc73d18e308
Jordan <jordan.tucker1@ibm.com>
2021-12-06 13:29:08 +0000

    optimise day 6
    Signed-off-by: Jordan <jordan.tucker1@ibm.com>

--- day6/bothParts.js
+++ day6/bothParts.js
@@ -4,45 +4,23 @@ const path = require('path');
 const inputFile = fs.readFileSync(path.resolve(__dirname, 'input.txt'), 'utf8');
 const input = inputFile.split(',').map(num => parseInt(num));
 
-const compressFish = (fish) => 
-  fish.reduce((compressed, fishAge) => {
-    if (!compressed[fishAge]) {
-      compressed[fishAge] = 1;
-    } else {
-      compressed[fishAge]++;
-    }
-    return compressed;
-  }, {});
-
-const countFish = (compressedFish) => 
-  Object.values(compressedFish).reduce((sum, count) => {
+const sum = (array) => 
+  Object.values(array).reduce((sum, count) => {
     sum+=count;
     return sum;
   }, 0);
 
-const tick = (compressedFish) => {
-  let newFish = 0;
-  Object.entries(compressedFish).forEach(([age, count]) => {
-    if(age == 0) {
-      newFish = count;
-    } else {
-      compressedFish[age - 1] = count;
-      compressedFish[age] = 0;
-    }
-  });
-
-  compressedFish[8] = newFish;
-  compressedFish[6] = compressedFish[6] ? compressedFish[6] + newFish : newFish;
-
-  return compressedFish;
-}
-
 const run = (input, days) => {
-  let compressedFish = compressFish(input);
+  const fish = new Array(9).fill(0);
+  input.forEach(num => {fish[num] += 1});
+
   for (let i = 1; i <= days; i++) {
-    compressedFish = tick(compressedFish);
+    newFish = fish.shift();
+    fish.push(newFish);
+    fish[6] += newFish
   }
-  console.log(`After ${days} days:`, countFish(compressedFish));
+
+  console.log(`After ${days} days:`, sum(fish));
 }
 
 run(input, 80);
